225aa1679c3d20609e98e1938a68707b7a30040f
Prakash <prakashk0301@gmail.com>
2019-02-03 18:10:25 +0530

    added deployment file

---
+++ deployment/deploy_prod.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+ssh root@18.184.123.248 <<EOF
+  cd djtrump
+  git pull
+  source /opt/envs/djtrump/bin/activate
+  pip install -r requirements.txt
+  ./manage.py migrate
+  sudo supervisorctl restart djtrump
+  exit
+EOF
