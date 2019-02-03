4c53b84e429431adcb245dcf9733f91642a0297d
prakashk0301 <34745120+prakashk0301@users.noreply.github.com>
2019-02-03 18:39:03 +0530

    Update deploy_prod.sh

--- deployment/deploy_prod.sh
+++ deployment/deploy_prod.sh
@@ -3,9 +3,9 @@
 ssh root@18.185.73.165 <<EOF
   cd project1
   git pull
-  source /opt/envs/djtrump/bin/activate
+  source /opt/envs/project1/bin/activate
   pip install -r requirements.txt
-  ./manage.py migrate
+  #./manage.py migrate
   sudo supervisorctl restart djtrump
   exit
 EOF
