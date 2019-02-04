a47ad07baaa104a407107213828e3b424e661837
prakashk0301 <34745120+prakashk0301@users.noreply.github.com>
2019-02-05 02:38:02 +0530

    Update deploy_prod.sh

--- deployment/deploy_prod.sh
+++ deployment/deploy_prod.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-ssh ec2-user@18.194.22.139
+ssh ec2-user@18.194.22.139 <<EOF
   cd project1
   git pull
   source /opt/envs/project1/bin/activate
@@ -11,4 +11,5 @@ ssh ec2-user@18.194.22.139
   service httpd enable
   #./manage.py migrate
   sudo supervisorctl restart djtrump
- 
+  exit
+EOF
