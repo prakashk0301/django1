0a94e7fb6bd423bbb1459e938363b0b57042e528
prakashk0301 <34745120+prakashk0301@users.noreply.github.com>
2019-02-05 02:45:12 +0530

    Update deploy_prod.sh

--- deployment/deploy_prod.sh
+++ deployment/deploy_prod.sh
@@ -5,11 +5,7 @@ ssh ec2-user@18.194.22.139 <<EOF
   git pull
   source /opt/envs/project1/bin/activate
   pip install -r requirements.txt
-  yum install -y update
-  yum install -y httpd
-  service httpd start 
-  service httpd enable
   #./manage.py migrate
-  sudo supervisorctl restart djtrump
+  sudo supervisorctl restart project1
   exit
 EOF
