f71b1e50f862ac2eaa67cefcbf4bcb54eae34376
prakashk0301 <34745120+prakashk0301@users.noreply.github.com>
2019-02-08 13:16:39 +0530

    url added for python 3.7

--- deployment/deploy_prod.sh
+++ deployment/deploy_prod.sh
@@ -3,6 +3,7 @@
 ssh root@142.93.116.229 <<EOF
   apt-get update -y
   #yum install update -y
+  # for python 3.7 https://tecadmin.net/install-python-3-7-on-ubuntu-linuxmint/
   git pull
   #git pull <url>
   #cd <project name>
