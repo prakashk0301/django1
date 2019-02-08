40fe6006bca1dc51a47acb29ee8f89912fca3b8d
prakashk0301 <34745120+prakashk0301@users.noreply.github.com>
2019-02-08 12:47:07 +0530

    added project name

--- deployment/deploy_prod.sh
+++ deployment/deploy_prod.sh
@@ -5,8 +5,9 @@ ssh root@142.93.116.229 <<EOF
   #yum install update -y
   git pull
   #git pull <url>
-  cd project1
-  source /opt/envs/project1/bin/activate
+  #cd <project name>
+  cd django1
+  source /opt/envs/django1/bin/activate
   pip install --upgrade pip
   pip install -r requirements.txt
   #./manage.py migrate
