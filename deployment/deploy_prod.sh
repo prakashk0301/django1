fadd7aca3ab8ac7c56d51aa2b5ebbfdd9164c5be
prakashk0301 <34745120+prakashk0301@users.noreply.github.com>
2019-02-08 12:25:23 +0530

    added webserver's ip

--- deployment/deploy_prod.sh
+++ deployment/deploy_prod.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-ssh root@142.93.116.229 <<EOF
-  apt-get update -y
+ssh root@54.93.39.57 <<EOF
+  yum update -y
   git pull
   cd project1
   source /opt/envs/project1/bin/activate
   pip install --upgrade pip
   pip install -r requirements.txt
   #./manage.py migrate
-  sudo service apache2 restart 
+  sudo service httpd restart 
   exit
 EOF
