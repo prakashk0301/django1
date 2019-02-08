20c2d97cbb3db3bc604dc727fa308cb921a57ec6
prakashk0301 <34745120+prakashk0301@users.noreply.github.com>
2019-02-08 07:31:46 +0530

    Update deploy_prod.sh

--- deployment/deploy_prod.sh
+++ deployment/deploy_prod.sh
@@ -1,8 +1,9 @@
-#!/bin/sh
+#!/bin/bashsh
 
 ssh root@142.93.116.229 <<EOF
-  cd project1
+  apt-get update -y
   git pull
+  cd project1
   source /opt/envs/project1/bin/activate
   pip install -r requirements.txt
   #./manage.py migrate
